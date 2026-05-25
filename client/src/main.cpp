10cc15d0c8ca3c853e76eccf31684c1f8891e6b2
rosie-mc <24433802@studentmail.ul.ie>
2026-05-25 13:42:58 +0100

    R1-07: Add HttpClient class with HTTPS GET via libcurl

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "HttpClient.h"
 
 int main() {
     std::cout << "SecureChat C++ Client v0.1" << std::endl;
+
+    HttpClient client;
+    std::string response = client.get("https://httpbin.org/get");
+    std::cout << "Response: " << response << std::endl;
+
     return 0;
 }
