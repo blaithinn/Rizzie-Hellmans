07971a29fb1988a2089a56f7242ab55ede4f4e66
rosie-mc <24433802@studentmail.ul.ie>
2026-05-25 13:55:03 +0100

    R1-08: HttpClient calls Blaithin's server health endpoint

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -5,7 +5,7 @@ int main() {
     std::cout << "SecureChat C++ Client v0.1" << std::endl;
 
     HttpClient client;
-    std::string response = client.get("https://httpbin.org/get");
+    std::string response = client.get("https://rizzie-hellmans.theburkenator.com/health");
     std::cout << "Response: " << response << std::endl;
 
     return 0;
